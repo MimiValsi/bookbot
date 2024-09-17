736fbd4ffebd372bbc1d3264e4b58e7e92894e42
Miguel Da Silva <miguel@dasilvaf.net>
2024-09-17 19:12:52 +0200

    Add function to total words instead of just len

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(len(text.split()))
+    words = total_number_words(text)
+    print(text.split())
+    print(f"{words} total words")
+
+
+def total_number_words(text):
+    return len(text.split())
 
 
 def get_book_text(path):
